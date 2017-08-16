905e98d28e64e4a61b5eb88dada5b7778988368f
kuanghuan <huankuang@alog.com>
2017-08-16 16:12:48 +0800

    add springcloud config client

--- config-client/src/main/java/com/xyz/spring/boot/cloud/SpringCloudConfigClientApplication.java
+++ config-client/src/main/java/com/xyz/spring/boot/cloud/SpringCloudConfigClientApplication.java
@@ -2,13 +2,12 @@ package com.xyz.spring.boot.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
-public class SpringCloudDemoApplication {
+public class SpringCloudConfigClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudDemoApplication.class, args);
+		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
 	}
+	
 }
